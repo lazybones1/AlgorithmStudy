668d960cb68b77d9cab421152cdd28b881a388cf
sukyung99 <201802067@o.cnu.kr>
2022-03-15 21:51:12 +0900

    add(Gold5/2075/김수경)

---
+++ 22_03_15/Gold5_2075_N번째 큰수/BJ2075_Sukyung.java
@@ -0,0 +1,29 @@
+package day0315;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BJ2075_Sukyung {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < N; j++) {
+				pq.offer(Integer.parseInt(st.nextToken()));
+
+			}
+		}
+		for (int i = 0; i < N - 1; i++) {
+			pq.poll();
+		}
+		System.out.println(pq.poll());
+	}
+}
