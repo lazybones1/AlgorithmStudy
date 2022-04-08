598f91a11aac8cadf556127fc7c868d8443413b6
seungh <seungh1024@naver.com>
2022-04-08 23:13:03 +0900

    add(Silver 1/1325/강승훈), (Silver 3/16439/강승훈) , (silver 5/9655/강승훈), (silver 3/2579/강승훈)

---
+++ 22_03_31/silver 5_9655_돌게임/9655_돌게임_강승훈.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N <= 3) {
+            if (N == 2) {
+                System.out.println("CY");
+            } else {
+
+                System.out.println("SK");
+            }
+            return;
+        }
+
+        int[] stone = new int[N + 1];
+        stone[1] = stone[3] = -1;
+
+        for (int i = 4; i <= N; i++) {
+            if (stone[i - 2] == -1 && stone[i - 4] == -1) {
+                stone[i] = -1;
+            }
+            if (i >= 6 && stone[i - 4] == -1 && stone[i - 6] == -1) {
+                stone[i] = -1;
+            }
+        }
+
+        // System.out.println(stone[N]);
+        // System.out.println(Arrays.toString(stone));
+        if (stone[N] == -1) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+
+        // if(N>3) {
+        // if(N%2 == 0) {
+        // System.out.println("CY");
+        // }else {
+        // System.out.println("SK")
+        // }
+        // }else {
+        // System.out.println("SK");
+        // }
+
+    }
+}
